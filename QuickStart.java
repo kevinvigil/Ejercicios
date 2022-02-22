b8f18ae937d3234504f46c5b75bd42c8bc7c8cf5
kevinVigil <kevinvigil27@gmail.com>
2022-02-22 15:22:57 -0300

    first commit

---
+++ QuickStart.java
@@ -0,0 +1,7 @@
+class QuickStart {
+    public static void main (String[] args) {
+        System.out.println("Hello, World.");
+        //Integrated: Enable File Links
+        //${task}${separator}${local}${separator}${cwdFolder}
+    }
+}
